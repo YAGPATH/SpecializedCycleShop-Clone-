b20f8e393b028689da8df5f710895ed343e13ffd
yagpath <vikasm027@gmail.com>
2023-07-12 23:05:37 +0530

    final push

--- Specialized Cycle clone/scripts/cart.js
+++ Specialized Cycle clone/scripts/cart.js
@@ -1,39 +1,30 @@
 let sumMRP = 0; // total MRP
 let data = JSON.parse(localStorage.getItem("cart-list")) || []; // Retrieve cart data from local storage or initialize as empty array if not found
 
-let cartItem = document.querySelector("#cart"); 
+let cartItem = document.querySelector("#cart");
 let sum = 0; // total price
 
 function displayCart(data) {
-  document.querySelector("#cart").textContent = ""; // Clear the cart element before displaying items
+ 
+  cartItem.innerHTML = ""; // Clear the cart element before displaying items
   console.log(data, sum);
-  data.map((ele, i) => {
-    
-
+  data.forEach((ele, i) => {
     let div = document.createElement("div"); // Create a div element for each cart item
     div.id = "mainDiv";
 
-    let divImg = document.createElement("div"); 
+    let divImg = document.createElement("div");
     divImg.id = "imgDiv";
 
-    let img = document.createElement("img"); 
-    
+    let img = document.createElement("img");
     img.src = ele.image;
 
-    let divDetails = document.createElement("div"); 
+    let divDetails = document.createElement("div");
     divDetails.id = "detail";
 
-    let productdescriptionname = document.createElement("h4"); 
+    let productdescriptionname = document.createElement("h4");
     productdescriptionname.textContent = ele.title;
 
-    // let code = document.createElement("p"); 
-    // code.textContent = "2456155";
-
-    // let MRP = document.createElement("p"); 
-    // MRP.textContent = "₹ " + ele.mrp;
-    // MRP.id = "displayMRPCart";
-
-    let mrp = document.createElement("p"); 
+    let mrp = document.createElement("p");
     mrp.textContent = "₹ " + ele.price;
     mrp.id = "displayMRPfynl";
 
@@ -42,65 +33,45 @@ function displayCart(data) {
 
     let disc = document.createElement("button"); // Decrease quantity button
     disc.textContent = "-";
-
-    let qnty = document.createElement("input"); 
-    qnty.value = 1;
-
-    let add = document.createElement("button"); // Increase quantity button
-    add.textContent = "+";
-
-    let divFinalmrp = document.createElement("div"); 
-
-    let finmrp = document.createElement("p"); // Final price
-    sum += parseInt(ele.price); // Update the total price
-    sumMRP += parseInt(ele.mrp); // Update the total MRP
-    finmrp.textContent = "₹ " + parseInt(ele.price) * qnty.value;
-
-    
     disc.addEventListener("click", function () {
-      if (qnty.value == 1) {
+      if (ele.quantity == 1) {
         alert("Minimum Quantity could be 1");
       } else {
-        qnty.value--;
-        sum -= parseInt(ele.price); 
-        sumMRP -= parseInt(ele.mrp); 
-        finmrp.textContent = "₹ " + parseInt(ele.price) * qnty.value;
-        localStorage.setItem("cart_Value", sum);
-
-        document.getElementById("displayMRP").textContent = "₹ " + sumMRP;
-        document.getElementById("finalAmt").textContent = "₹ " + sum;
+        ele.quantity--;
+        qnty.value = ele.quantity;
+        updatePrice();
       }
     });
 
-    
-    add.addEventListener("click", function () {
-      qnty.value++;
-      sum += parseInt(ele.price); 
-      sumMRP += parseInt(ele.mrp); 
-      finmrp.textContent = "₹ " + parseInt(ele.price) * qnty.value;
+    let qnty = document.createElement("input");
+    qnty.value = ele.quantity || 1; // Initialize quantity to 1 if not previously set
 
-      document.getElementById("displayMRP").textContent = "₹ " + sumMRP;
-      document.getElementById("finalAmt").textContent = "₹ " + sum;
-      localStorage.setItem("cart_Value", sum);
+    let add = document.createElement("button"); // Increase quantity button
+    add.textContent = "+";
+    add.addEventListener("click", function () {
+      ele.quantity++;
+      qnty.value = ele.quantity;
+      updatePrice();
     });
 
-    let delet = document.createElement("div"); 
+    let divFinalmrp = document.createElement("div");
+
+    let finmrp = document.createElement("p"); // Final price
+    finmrp.textContent = "₹ " + parseInt(ele.price * (ele.quantity || 1)); // Calculate the final price using quantity or default to 1
+    finmrp.id = "finalPrice";
+
+    let delet = document.createElement("div");
     let del = document.createElement("button"); // Delete button
     del.id = "delcart";
     let delimg = document.createElement("img"); // Delete button icon
     delimg.src = "../Images/delet.svg";
     delimg.id = "dellogo";
 
-    
     del.addEventListener("click", function () {
       data.splice(i, 1); // Remove the item from the cart data
-      sum -= parseInt(ele.price); 
-      sumMRP -= parseInt(ele.mrp); 
-      localStorage.setItem("cart", JSON.stringify(data));
-
-      document.getElementById("displayMRP").textContent = "₹ " + sumMRP;
-      document.getElementById("finalAmt").textContent = "₹ " + sum;
-      displayCart(data); 
+      updatePrice();
+      localStorage.setItem("cart-list", JSON.stringify(data));
+      displayCart(data);
     });
 
     divImg.append(img);
@@ -111,16 +82,37 @@ function displayCart(data) {
     delet.append(del);
     div.append(divImg, divDetails, qty, divFinalmrp, delet);
     cartItem.append(div);
-
-    localStorage.setItem("cart_Value", sum);
-
-    document.getElementById("displayMRP").textContent = "₹ " + sumMRP;
-    document.getElementById("finalAmt").textContent = "₹ " + sum;
   });
+
+  updatePrice();
 }
 
-displayCart(data); 
+function updatePrice() {
+  sum = 0;
+  sumMRP = 0;
+  let cartItems = document.querySelectorAll("#cart #mainDiv");
+  cartItems.forEach((item) => {
+    let price = parseInt(
+      item
+        .querySelector("#detail #displayMRPfynl")
+        .textContent.replace("₹ ", "")
+    );
+    let quantity = parseInt(item.querySelector(".qtyDiv input").value) || 1; // Retrieve quantity or default to 1
+    let finalPrice = price * quantity;
+    item.querySelector("#finalPrice").textContent = "₹ " + finalPrice;
+    sum += finalPrice;
+    sumMRP += price;
+    item.dataset.quantity = quantity; // Update the quantity value in the dataset attribute
+  });
+
+  localStorage.setItem("cart_Value", sum);
+  document.getElementById("displayMRP").textContent = "₹ " + sumMRP;
+  document.getElementById("finalAmt").textContent = "₹ " + sum;
+  localStorage.setItem("cart-list", JSON.stringify(data)); // Update the cart data in local storage with the updated quantity values
+}
+
+displayCart(data);
 
 function home() {
-  location.href = "../index.html"; 
+  location.href = "../index.html";
 }
--- Specialized Cycle clone/scripts/wishlist.js
+++ Specialized Cycle clone/scripts/wishlist.js
@@ -1,31 +1,18 @@
-
-// main code starts here-----------------------
-
-// let wishlistData = [];
-
 let wishlistData = JSON.parse(localStorage.getItem("wishlist")) || [];
-// displayData(wishlistData);
-console.log(wishlistData,"hi...");
+let cartData = JSON.parse(localStorage.getItem("cart-list")) || [];
+let addedToCartData = JSON.parse(localStorage.getItem("added-to-cart")) || [];
 
-
-//catching container
 let WishlistProducts = document.getElementById("WishlistItemsTable");
-let Price;
+
 const displayData = (data) => {
   WishlistProducts.innerHTML = "";
   data.map((ele, index) => {
-    console.log(data);
-
     let row = document.createElement("div");
     row.setAttribute("id", "row");
-
-    // let col1 = document.createElement("d");
 
     let img = document.createElement("img");
     img.src = ele.image;
     img.setAttribute("class", "WishlistImage");
-
-    // col1.append(img);
 
     let col2 = document.createElement("div");
     col2.setAttribute("id", "secondDiv");
@@ -48,38 +35,42 @@ const displayData = (data) => {
     let div = document.createElement("div");
     div.setAttribute("id", "btnForWishList");
 
-
-    //Added col two elements
     col2.append(innDiv, div);
 
     let col3 = document.createElement("div");
     col3.setAttribute("id", "thirdDiv");
 
-    Price = document.createElement("p");
-
+    let Price = document.createElement("p");
     Price.innerText = `₹ ${ele.price}`;
-    console.log(Price);
 
     col3.append(Price);
 
     let col4 = document.createElement("div");
     col4.setAttribute("id", "WishlistBtns");
     let addToCartListBtn = document.createElement("button");
-    addToCartListBtn.innerText = "Add to Cart";
+    let isInCart = cartData.some((item) => item.id === ele.id);
+
+    if (isInCart) {
+      addToCartListBtn.innerText = "ADDED ✔";
+      addToCartListBtn.disabled = true;
+    } else {
+      addToCartListBtn.innerText = "Add to Cart";
+      addToCartListBtn.disabled = false;
+    }
 
     addToCartListBtn.addEventListener("click", () => {
       addToCartListBtn.innerText = "ADDING...";
-    setTimeout(function() {
-      addToCartListBtn.innerText = "ADDED ✔";        
-    }, 1000);
-      addToCartList(ele);
+      addToCartListBtn.disabled = true;
+      addToCartList(ele, index);
     });
+
     let iconHeart = document.createElement("span");
     iconHeart.setAttribute("id", "removeFromWish");
-    iconHeart.src  = "../Images/delet.svg";
+    iconHeart.innerHTML = '<i class="fa fa-trash"></i>';
     iconHeart.addEventListener("click", () => {
       getDelete(index);
     });
+
     col4.append(addToCartListBtn, iconHeart);
 
     row.append(img, col2, col3, col4);
@@ -87,22 +78,26 @@ const displayData = (data) => {
     WishlistProducts.append(row);
   });
 };
+
 displayData(wishlistData);
-//Delete Func
+
 const getDelete = (index) => {
   wishlistData.splice(index, 1);
   localStorage.setItem("wishlist", JSON.stringify(wishlistData));
   displayData(wishlistData);
 };
-//Adding item to Cartlist
-function addToCartList(el) {
-  console.log(el);
-  let cartData = JSON.parse(localStorage.getItem("cart-list")) || [];
+
+function addToCartList(el, index) {
   cartData.push(el);
   localStorage.setItem("cart-list", JSON.stringify(cartData));
-  
+  addedToCartData.push(el.id);
+  localStorage.setItem("added-to-cart", JSON.stringify(addedToCartData));
+
+  let addToCartBtn = WishlistProducts.children[index].querySelector("button");
+  addToCartBtn.innerText = "ADDED ✔";
+  addToCartBtn.disabled = true;
 }
 
-function home(){
-  location.href="index.html"
+function home() {
+  location.href = "index.html";
 }
